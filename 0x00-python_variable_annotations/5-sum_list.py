ad66b50e811049c5b7f9baf6209a650c399c3a8f
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-07-05 14:43:44 +0100

    annotations in python

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """sums a float number"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
    """returns the sum of float numbers"""
    sumlist: float = 0.0
    for value in input_list:
