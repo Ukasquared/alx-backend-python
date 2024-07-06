b127b48e86ac716fc1f1a3895f2c19f4d807ac75
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-07-06 05:28:22 +0100

    anotations in python

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """module make_multiplier"""
-from typing import Callable, Union
+from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> Callable[Union[float], float]:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """module make_multiplier"""
     def func(x: float) -> float:
         """ module"""
