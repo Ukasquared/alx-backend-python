cfc7aba28fff9430ae2c5be0f2aa7a81a8f4e82e
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-07-06 05:25:46 +0100

    anotations in python

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,4 +4,5 @@ from typing import List, Iterable, Tuple, Sequence
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """module iterable """
     return [(i, len(i)) for i in lst]
