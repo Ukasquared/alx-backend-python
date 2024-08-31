61b1779aaabdfe1a094ec15a824695f6527f8ab0
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-08-31 06:00:28 +0100

    unitest

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,9 @@
 
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import Mock, patch
+
 
 class TestAccessNestedMap(unittest.TestCase):
     """ test the utils module"""
@@ -26,6 +28,30 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError):
             access_nested_map(m, p)
 
+
+class TestGetJson(unittest.TestCase):
+    """testing http request """
+
+    @patch('request.get')
+    def test_get_json(self, mock_it):
+        """ test an http request """
+        test_cases = [
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False}),
+        ]
+
+        for test_url, test_payload in test_cases:
+            # Configure the mock to return the test_payload
+            mock_get.return_value.json.return_value = test_payload
+
+            # Call the function
+            result = get_json(test_url)
+
+            # Assert the mocked get method was called exactly once with the test_url
+            mock_get.assert_called_once_with(test_url)
+
+            # Assert that the output is equal to test_payload
+            self.assertEqual(result, test_payload)
 
 if __name__ == "__main__":
     unittest.main()
